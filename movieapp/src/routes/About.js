e0e2968f82604f21c62659b8da4e9686684c552d
YEONGEOL <dusrjf87@gmail.com>
2021-03-01 20:37:33 +0900

    movie app 끝!

---
+++ movieapp/src/routes/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return (
+    <span>About this page: I built it because i love movies</span>
+  );
+}
+
+export default About;
